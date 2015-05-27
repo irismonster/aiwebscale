3164ea22f000feed327be13a682d112a292d1dfc
Iris Monster <irismonster@mai-ai.org>
2015-05-27 22:33:36 +0200

    Merge conflict oplossen

--- practical/basics/rest_interaction.py
+++ practical/basics/rest_interaction.py
@@ -1,13 +1,18 @@
 __author__ = 'tessa'
+import urllib.request
+import json
 
 class RestInteraction:
 
     def __init__(self):
         return
 
-    def getcontext(self):
+    def getcontext(self):           
         return
 
     def proposepage(self):
         print('')
         return
+
+r = RestInteraction()
+r.getcontext()

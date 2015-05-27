25834ddf09ddc81345c5cbcd9ffe532d2509dfbe
Tessa <tessa.beinema@hotmail.com>
2015-05-27 12:22:25 +0200

    Poging 2

--- practical/basics/rest_interaction.py
+++ practical/basics/rest_interaction.py
@@ -1,1 +1,13 @@
 __author__ = 'tessa'
+
+class RestInteraction:
+
+    def __init__(self):
+        return
+
+    def getcontext(self):
+        return
+
+    def proposepage(self):
+        print('')
+        return

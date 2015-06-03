55ab614abdfcb666d88e99af42a68309b6138d68
Tessa <tessa.beinema@hotmail.com>
2015-06-03 13:34:44 +0200

    Made TeamID and TeamPassword  global. Added a simple 'calculate revenue function'.

--- practical/basics/rest_interaction.py
+++ practical/basics/rest_interaction.py
@@ -5,8 +5,12 @@ import json
 
 class RestInteraction:
 
-    def __init__(self):
-        return
+    teamid = None
+    teampw = None
+
+    def __init__(self, teamid, teampw):
+        self.teamid = teamid
+        self.teampw = teampw
 
     def loopOverPeople(self, runID, N): # RunID is quite obvious, N is number of people, so the i's
         for i in range(N):
@@ -14,7 +18,7 @@ class RestInteraction:
         return
 
     def getcontext(self, runID, i):
-        res = urllib.request.urlopen("http://krabspin.uci.ru.nl/getcontext.json/?i="+str(i)+"&runid="+str(runID)+"&teamid=Error_Teamname_not_found&teampw=845ea0b18db9a82bc43c811d740d3177").read()
+        res = urllib.request.urlopen("http://krabspin.uci.ru.nl/getcontext.json/?i="+str(i)+"&runid="+str(runID)+"&teamid=" + self.teamid + "&teampw=" + self.teampw).read()
         obj = json.loads(res.decode())  # Example: {'context': {'ID': 236, 'Age': 42.0, 'Agent': 'mobile', 'Referer': 'Bing', 'Language': 'EN'}}
         context = obj.get('context')    # Example: {'ID': 236, 'Age': 42.0, 'Agent': 'mobile', 'Referer': 'Bing', 'Language': 'EN'}
         age = context.get('Age')        # Example: 42.0
@@ -26,26 +30,32 @@ class RestInteraction:
 
 
     def proposepage(self, ):
-        teamid = 'Error_Teamname_not_found'
-        teampw = '845ea0b18db9a82bc43c811d740d3177'
         runid = '10'
-        i = '3'
+        i = '1'
         header = '5' #5, 15 or 35
-        adtype = 'banner' #skyscraper, square or banner
-        color = 'black' #green, blue, red, black, white
+        adtype = 'skyscraper' #skyscraper, square or banner
+        color = 'green' #green, blue, red, black, white
         productid = '10' #10-25
-        price = '10' #0-50
+        price = 1 #0-50
 
-        url = 'http://krabspin.uci.ru.nl/proposePage.json/?' + 'i=' + i + '&runid=' + runid + '&teamid=' + teamid \
+        url = 'http://krabspin.uci.ru.nl/proposePage.json/?' + 'i=' + i + '&runid=' + runid + '&teamid=' + self.teamid \
               + '&header=' + header + '&adtype=' + adtype + '&color=' + color + '&productid=' + productid \
-              + '&price=' + price + '&teampw=' + teampw
+              + '&price=' + str(price) + '&teampw=' + self.teampw
         res = urllib.request.urlopen(url).read()
         obj = json.loads(res.decode())
 
         success = obj.get('effect').get('Success')
         error = obj.get('effect').get('Error')
         if error != None:
-            print('PANIEK!')
+            print('!!! An error was given in the propose page function !!!')
+            return
+
+        print('You asked ' + str(price) + ' euro.')
+        print('Your revenue was: ' + str(self.compute_revenue_single_user(price, success)))
 
         return success, error
+
+    def compute_revenue_single_user(self, price, success):
+        return price*success
+
 
